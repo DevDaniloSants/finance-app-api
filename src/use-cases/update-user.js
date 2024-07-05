f837256f63ed9198083bd2948866b5865366f025
Danilo Santos <dscofield@outlook.com.br>
2024-07-04 21:24:46 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+
+import { EmailIsAlreadyInUseError } from '../errors/user.js';
+import { PostgresGetByUserEmail } from '../repositories/postgres/get-user-by-email.js';
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js';
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetByUserEmail = new PostgresGetByUserEmail();
+
+            const userWithProvidedEmail = postgresGetByUserEmail.execute(
+                updateUserParams.email,
+            );
+
+            if (userWithProvidedEmail) {
+                throw new EmailIsAlreadyInUseError(updateUserParams.email);
+            }
+        }
+
+        const user = { ...updateUserParams };
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            );
+
+            user.password = hashedPassword;
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        );
+
+        return updatedUser;
+    }
+}
