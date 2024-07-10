00882ea63066ae1dee8bd32bcd7dea74d1e97961
Danilo Santos <dscofield@outlook.com.br>
2024-07-10 20:29:31 -0300

    feat: add create transaction use case

--- src/errors/user.js
+++ src/errors/user.js
@@ -4,3 +4,10 @@ export class EmailIsAlreadyInUseError extends Error {
         this.name = 'EmailIsAlreadyInUseError';
     }
 }
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`User with id ${userId} not found.`);
+        this.name = 'UserNotFoundError ';
+    }
+}
