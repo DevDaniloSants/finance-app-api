330b6c2bb570bbd25ca34e7da73af4bf171c1d24
Danilo Santos <dscofield@outlook.com.br>
2024-07-16 20:48:40 -0300

    fix: retrieve transactions by user_id

--- src/repositories/postgres/transaction/get-transactions-by-userId.js
+++ src/repositories/postgres/transaction/get-transactions-by-userId.js
@@ -4,11 +4,11 @@ export class GetTransactionsByUserIdRepository {
     async execute(userId) {
         const transactions = await PostgresHelper.query(
             `
-        SELECT * FROM transactions WHERE user_id = $1 RETURNING * 
+        SELECT * FROM transactions WHERE user_id = $1 
         `,
             [userId],
         );
 
-        return transactions[0];
+        return transactions;
     }
 }
