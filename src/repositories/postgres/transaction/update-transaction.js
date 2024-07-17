6381c6d5a63a376da2fb09f56aec16f7134da280
Danilo Santos <dscofield@outlook.com.br>
2024-07-16 21:13:11 -0300

    feat: add updateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,27 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js';
+
+export class UpdateTransactionRepository {
+    async execute(transactionId, updateTransactionParams) {
+        const updateFields = [];
+        const updateValues = [];
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`);
+            updateValues.push(updateTransactionParams[key]);
+        });
+
+        updateValues.push(transactionId);
+
+        const updateQuery = `
+        UPDATE transactions SET ${updateFields.join(', ')}
+        WHERE id = $${updateValues.length}
+        `;
+
+        const updateTransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        );
+
+        return updateTransaction[0];
+    }
+}
