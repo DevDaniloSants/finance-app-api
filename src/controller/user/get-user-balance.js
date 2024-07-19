ad9b9ad015ff7db9842bb7d587a95de39c3dcd26
Danilo Santos <dscofield@outlook.com.br>
2024-07-18 23:42:09 -0300

    fix: add missing await to getUserBalanceUseCase execution

--- src/controller/user/get-user-balance.js
+++ src/controller/user/get-user-balance.js
@@ -22,7 +22,9 @@ export class GetUserBalanceController {
                 return invalidIdResponse();
             }
 
-            const balance = this.getUserBalanceUseCase.execute({ userId });
+            const balance = await this.getUserBalanceUseCase.execute({
+                userId,
+            });
 
             return ok(balance);
         } catch (error) {
