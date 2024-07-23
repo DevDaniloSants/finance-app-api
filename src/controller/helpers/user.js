3ab5fb00066b7acd6c69020815a8b3a7669bca78
Danilo Santos <dscofield@outlook.com.br>
2024-07-23 20:11:59 -0300

    feat: add zod to UpdateUserController

--- src/controller/helpers/user.js
+++ src/controller/helpers/user.js
@@ -1,21 +1,5 @@
-import validator from 'validator';
-
 import { badRequest } from './http.js';
 
 export const userNotFoundResponse = () => {
     return badRequest({ message: 'The user was not found.' });
 };
-
-export const invalidPasswordResponse = () =>
-    badRequest({
-        message: 'Password must be at Least 6 characters',
-    });
-
-export const invalidEmailResponse = () =>
-    badRequest({
-        message: 'Invalid Email: Please provide a valid one',
-    });
-
-export const checkIfPasswordIsValid = (password) => password.length >= 6;
-
-export const checkIfEmailIsValid = (email) => validator.isEmail(email);
--- src/controller/user/update-user.js
+++ src/controller/user/update-user.js
@@ -1,11 +1,12 @@
+import { ZodError } from 'zod';
+
 import { EmailIsAlreadyInUseError } from '../../errors/user.js';
+
+import { updateUserSchema } from '../../schemas/user.js';
+
 import {
-    checkIfEmailIsValid,
     checkIfIdIsValid,
-    checkIfPasswordIsValid,
-    invalidEmailResponse,
     invalidIdResponse,
-    invalidPasswordResponse,
     badRequest,
     ok,
     serverError,
@@ -27,38 +28,7 @@ export class UpdateUserController {
 
             const params = httpRequest.body;
 
-            const allowedFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ];
-
-            const someFieldIsNotAllowed = Object.keys(params).some(
-                (field) => !allowedFields.includes(field),
-            );
-
-            if (someFieldIsNotAllowed) {
-                return badRequest({
-                    message: 'Some provided field is not allowed.',
-                });
-            }
-
-            if (params.password) {
-                const passwordIsValid = checkIfPasswordIsValid(params.password);
-
-                if (!passwordIsValid) {
-                    return invalidPasswordResponse();
-                }
-            }
-
-            if (params.email) {
-                const emailIsValid = checkIfEmailIsValid(params.email);
-
-                if (!emailIsValid) {
-                    return invalidEmailResponse();
-                }
-            }
+            await updateUserSchema.parseAsync(params);
 
             const updatedUser = await this.updateUserUseCase.execute(
                 userId,
@@ -67,6 +37,9 @@ export class UpdateUserController {
 
             return ok(updatedUser);
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({ message: error.errors[0].message });
+            }
             if (error instanceof EmailIsAlreadyInUseError) {
                 return badRequest({ message: error.message });
             }
