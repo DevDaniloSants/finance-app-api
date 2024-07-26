f47e697820c7f8bc1f8749fdf6e5dfb65c61a866
Danilo Santos <dscofield@outlook.com.br>
2024-07-25 21:58:04 -0300

    feat: ensure updateUserRepository use prisma

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,28 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class UpdateUserRepository {
     async execute(userId, updateUserParams) {
-        const updateFields = [];
-        const updateValues = [];
-
-        Object.keys(updateUserParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateValues.length + 1}`);
-            updateValues.push(updateUserParams[key]);
+        return await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: updateUserParams,
         });
-
-        updateValues.push(userId);
-
-        const updateQuery = `
-        UPDATE users SET ${updateFields.join(', ')} 
-        WHERE id = $${updateValues.length}
-        RETURNING *
-        `;
-
-        const updateUser = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        );
-
-        return updateUser[0];
     }
 }
