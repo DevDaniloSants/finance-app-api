c04bcb74ff92271d172bd700ce6be4cd12f96c1d
Danilo Santos <dscofield@outlook.com.br>
2024-07-29 20:43:58 -0300

    feat: add deleteTransactionRepository and transactionNotFoundError

---
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -0,0 +1,24 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { prisma } from '../../../../prisma/prisma.js';
+import { TransactionNotFoundError } from '../../../errors/transaction.js';
+
+export class DeleteTransactionRepository {
+    async execute(transactionId) {
+        try {
+            return await prisma.transaction.delete({
+                where: {
+                    id: transactionId,
+                },
+            });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
+                if (error.code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId);
+                }
+            }
+
+            throw error;
+        }
+    }
+}
