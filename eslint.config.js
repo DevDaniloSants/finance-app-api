dc9de3c270a1c687380dca2009b803947a450648
Danilo Santos <dscofield@outlook.com.br>
2024-08-01 20:31:36 -0300

    fix: ensures the correct functioning of ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,9 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
+    { languageOptions: { globals: { ...globals.node, ...globals.mocha } } },
     pluginJs.configs.recommended,
     {
         files: ['tests/**'],
-        languageOptions: { globals: { ...globals.node, ...globals.mocha } },
     },
 ];
