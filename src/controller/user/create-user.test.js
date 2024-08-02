8df4ae66caf83c59c3308cc067803c9eac08c1ac
Danilo Santos <dscofield@outlook.com.br>
2024-08-01 21:38:13 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -32,4 +32,51 @@ describe('create user controller', () => {
         expect(result.body).toBe(httpRequest.body);
         expect(result.body).not.toBeUndefined();
     });
+
+    it('should return 400 if first_name is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                last_name: 'Santos',
+                email: 'danilo@dnl.com',
+                password: '1234567',
+            },
+        };
+
+        // act
+
+        const result = await createUserController.execute(httpRequest);
+
+        //assert
+        expect(result.statusCode).toBe(400);
+        expect(result.body.first_name).toBeUndefined();
+    });
+    it('should return 400 if last_name is not provided', async () => {
+        //arrange
+
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                email: 'danilo@dnl.com',
+                password: '1234567',
+            },
+        };
+
+        // act
+
+        const result = await createUserController.execute(httpRequest);
+        //assert
+        expect(result.statusCode).toBe(400);
+        expect(result.body.last_name).toBeUndefined();
+    });
 });
