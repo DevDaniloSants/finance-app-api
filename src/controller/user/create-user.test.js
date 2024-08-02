b701bcff34673f068a5974298c4917740aa52551
Danilo Santos <dscofield@outlook.com.br>
2024-08-02 20:35:26 -0300

    test: ensure CreateUserUseCase is called with correct params

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -29,7 +29,7 @@ describe('create user controller', () => {
 
         // assert
         expect(result.statusCode).toBe(201);
-        expect(result.body).toBe(httpRequest.body);
+        expect(result.body).toEqual(httpRequest.body);
         expect(result.body).not.toBeUndefined();
     });
 
@@ -164,5 +164,32 @@ describe('create user controller', () => {
 
         //assert
         expect(result.statusCode).toBe(400);
+    });
+
+    it('should call CreateUserUseCase with correct params', async () => {
+        //arrange
+
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: 'Danilo',
+                last_name: 'Santos',
+                email: 'danilo@dnl.com',
+                password: '123456',
+            },
+        };
+
+        const executeSpy = jest.spyOn(createUserUseCase, 'execute');
+
+        //act
+
+        await createUserController.execute(httpRequest);
+
+        //assert
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
     });
 });
