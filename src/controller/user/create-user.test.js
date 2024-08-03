14c79a73c1305129985bf7788c808807c56a34aa
Danilo Santos <dscofield@outlook.com.br>
2024-08-03 15:33:28 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws

--- src/controller/user/create-user.test.js
+++ src/controller/user/create-user.test.js
@@ -194,4 +194,32 @@ describe('create user controller', () => {
         //assert
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
     });
+
+    it('', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 6 }),
+            },
+        };
+
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error();
+        });
+
+        //act
+
+        const result = await createUserController.execute(httpRequest);
+
+        //assert
+        expect(result.statusCode).toBe(500);
+    });
 });
