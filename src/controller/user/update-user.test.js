414a47e907f19f8e7144da5ae7cd1d9b87789c72
Danilo Santos <dscofield@outlook.com.br>
2024-08-05 21:15:01 -0300

    test: ensure UpdateUserController returns 400 if an unallowed field is provided

--- src/controller/user/update-user.test.js
+++ src/controller/user/update-user.test.js
@@ -75,4 +75,19 @@ describe('UpdateUserController', () => {
         //assert
         expect(result.statusCode).toBe(400);
     });
+    it('should return 400 when an unallowed field is provided', async () => {
+        //arrange
+        const { sut } = makeSut();
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                unallowed_field: 'unallowed_field',
+            },
+        });
+
+        //assert
+        expect(result.statusCode).toBe(400);
+    });
 });
