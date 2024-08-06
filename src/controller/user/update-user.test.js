80d351306d43cd04e4cd0c75e749799869fc6332
Danilo Santos <dscofield@outlook.com.br>
2024-08-05 21:10:15 -0300

    test: ensure UpdateUserController returns 400 if an invalid email is provided

--- src/controller/user/update-user.test.js
+++ src/controller/user/update-user.test.js
@@ -47,4 +47,17 @@ describe('UpdateUserController', () => {
         //assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('should return 400 when invalid email is provided', async () => {
+        //arrange
+        const { sut } = makeSut();
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, email: 'invalid_email' },
+        });
+
+        //assert
+        expect(result.statusCode).toBe(400);
+    });
 });
