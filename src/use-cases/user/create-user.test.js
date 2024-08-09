dd27c5848046c41dd8140a1df392551ceed5a3eb
Danilo Santos <dscofield@outlook.com.br>
2024-08-09 17:46:06 -0300

    test: ensure CreateUserUseCase throws if IdGeneratorAdapter  throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -150,4 +150,19 @@ describe('CreateUserUseCase', () => {
         //assert
         await expect(promise).rejects.toThrow();
     });
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        //arrange
+
+        const { sut, idGeneratorAdapter } = makeSut();
+        jest.spyOn(idGeneratorAdapter, 'execute').mockRejectedValueOnce(
+            new Error(),
+        );
+
+        //act
+        const promise = sut.execute(user);
+
+        //assert
+        await expect(promise).rejects.toThrow();
+    });
 });
