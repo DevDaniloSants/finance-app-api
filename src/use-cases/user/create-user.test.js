b462103af95a65131c3357eef186d90bae917937
Danilo Santos <dscofield@outlook.com.br>
2024-08-09 17:47:54 -0300

    test: ensure CreateUserUseCase throws if PasswordHasherAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -165,4 +165,19 @@ describe('CreateUserUseCase', () => {
         //assert
         await expect(promise).rejects.toThrow();
     });
+
+    it('should throw if PasswordHasherAdapter throws', async () => {
+        //arrange
+
+        const { sut, passwordHasherAdapter } = makeSut();
+        jest.spyOn(passwordHasherAdapter, 'execute').mockRejectedValueOnce(
+            new Error(),
+        );
+
+        //act
+        const promise = sut.execute(user);
+
+        //assert
+        await expect(promise).rejects.toThrow();
+    });
 });
