218004a86c2b6770af14a0a376d09ba7450f3d8e
Danilo Santos <dscofield@outlook.com.br>
2024-08-14 20:33:16 -0300

    test: ensure DeleteTransactionUseCase deletes a transaction

---
+++ src/use-cases/transaction/delete-transaction.test.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import { DeleteTransactionUseCase } from './delete-transaction';
+
+describe('DeleteTransactionUseCase', () => {
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.commerce.productName,
+        date: faker.date.anytime(),
+        type: faker.helpers.arrayElement(['EARNING', 'EXPENSE', 'INVESTMENT']),
+        amount: faker.finance.amount(),
+    };
+
+    class DeleteTransactionRepositoryStub {
+        async execute() {
+            return transaction;
+        }
+    }
+
+    const makeSut = () => {
+        const deleteTransactionRepository =
+            new DeleteTransactionRepositoryStub();
+        const sut = new DeleteTransactionUseCase(deleteTransactionRepository);
+
+        return { sut, deleteTransactionRepository };
+    };
+
+    it('should successfully delete a transaction', async () => {
+        //arrange
+        const { sut } = makeSut();
+
+        //act
+        const result = await sut.execute(transaction.id);
+
+        //assert
+        expect(result).toEqual(transaction);
+    });
+});
