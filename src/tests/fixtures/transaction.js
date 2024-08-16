8dc7b0f834bb8a8798e8dd57a24ba44c407f923f
Danilo Santos <dscofield@outlook.com.br>
2024-08-16 20:03:29 -0300

    fix: update transaction fields to use correct faker methods and parameters

--- src/tests/fixtures/transaction.js
+++ src/tests/fixtures/transaction.js
@@ -3,8 +3,8 @@ import { faker } from '@faker-js/faker';
 export const transaction = {
     id: faker.string.uuid(),
     user_id: faker.string.uuid(),
-    name: faker.commerce.productName,
-    date: faker.date.anytime(),
+    name: faker.commerce.productName(),
+    date: faker.date.anytime().toISOString(),
     type: faker.helpers.arrayElement(['EARNING', 'EXPENSE', 'INVESTMENT']),
-    amount: faker.finance.amount(),
+    amount: +faker.finance.amount({ min: 0, max: 10000, dec: 2 }),
 };
