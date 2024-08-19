edfb1552d0701d1569c355ae4d284ea94b2a0cb7
Danilo Santos <dscofield@outlook.com.br>
2024-08-19 20:46:15 -0300

    chore: add jest global setup file to run test db container docker before all tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@ const config = {
     coverageProvider: 'v8',
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.js'],
+    globalSetup: '<rootDir>/jest.global-setup.mjs',
 };
 
 export default config;
