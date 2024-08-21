fd7a5b9da861887351991e0d659ddd4c10db648f
Danilo Santos <dscofield@outlook.com.br>
2024-08-21 19:03:34 -0300

    test: ensure CreateUserRepository creates a user on db

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -2,7 +2,7 @@ import { user } from '../../../tests';
 import { CreateUserRepository } from './create-user';
 
 describe('CreateUserRepository', () => {
-    it('should successfully create an user', async () => {
+    it('should create a user on db', async () => {
         //arrange
         const sut = new CreateUserRepository();
 
@@ -10,6 +10,10 @@ describe('CreateUserRepository', () => {
         const result = await sut.execute(user);
 
         //assert
-        expect(result).not.toBeNull();
+        expect(result.id).toBe(user.id);
+        expect(result.first_name).toBe(user.first_name);
+        expect(result.last_name).toBe(user.last_name);
+        expect(result.email).toBe(user.email);
+        expect(result.password).toBe(user.password);
     });
 });
