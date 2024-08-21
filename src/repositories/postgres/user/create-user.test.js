aecea73aa331ba856362f33588eaa8e1bc47a73f
Danilo Santos <dscofield@outlook.com.br>
2024-08-21 19:10:40 -0300

    test: ensure CreateUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -1,3 +1,4 @@
+import { prisma } from '../../../../prisma/prisma';
 import { user } from '../../../tests';
 import { CreateUserRepository } from './create-user';
 
@@ -15,5 +16,15 @@ describe('CreateUserRepository', () => {
         expect(result.last_name).toBe(user.last_name);
         expect(result.email).toBe(user.email);
         expect(result.password).toBe(user.password);
+    });
+    it('should call Prisma with correct params', async () => {
+        //arrange
+        const sut = new CreateUserRepository();
+        const prismaSpy = jest.spyOn(prisma.user, 'create');
+        //act
+        await sut.execute(user);
+
+        //assert
+        expect(prismaSpy).toHaveBeenCalledWith({ data: user });
     });
 });
