16f24dffe1d6a04ce7bc2e02010f6a463d50c879
Danilo Santos <dscofield@outlook.com.br>
2024-08-30 21:37:47 -0300

    test: ensure MakeUpdateUserController returns valid UpdateUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,8 +1,11 @@
-import { CreateUserController } from '../../controller';
-import { makeCreateUserController } from './user';
+import { CreateUserController, UpdateUserController } from '../../controller';
+import { makeCreateUserController, makeUpdateUserController } from './user';
 
 describe('User Controller Factories', () => {
     it('should return a valid MakeCreateUserController instance', () => {
         expect(makeCreateUserController()).toBeInstanceOf(CreateUserController);
+    });
+    it('should return a valid MakeUpdateUserController instance', () => {
+        expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController);
     });
 });
