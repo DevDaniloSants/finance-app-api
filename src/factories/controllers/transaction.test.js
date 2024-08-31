dbf33f8aef59e96ea91f9d749b6b2b470158f85e
Danilo Santos <dscofield@outlook.com.br>
2024-08-30 21:31:28 -0300

    test: ensure GetTransactionsByUserIdController returns valid GetTransactionsByUserIdController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,21 @@
-import { CreateTransactionController } from '../../controller';
-import { makeCreateTransactionController } from './transaction';
+import {
+    CreateTransactionController,
+    GetTransactionsByUserIdController,
+} from '../../controller';
+import {
+    makeCreateTransactionController,
+    makeGetTransactionsByUserIdController,
+} from './transaction';
 
 describe('Transaction Controller Factories', () => {
     it('should return a valid CreateTransactionController instance', () => {
         expect(makeCreateTransactionController()).toBeInstanceOf(
             CreateTransactionController,
+        );
+    });
+    it('should return a valid GetTransactionsByUserIdController instance', () => {
+        expect(makeGetTransactionsByUserIdController()).toBeInstanceOf(
+            GetTransactionsByUserIdController,
         );
     });
 });
