0b8542be4d1dcba8d0d4401eb81a455d8a43d5e1
Danilo Santos <dscofield@outlook.com.br>
2024-08-30 21:33:14 -0300

    test: ensure MakeUpdateTransactionController returns valid UpdateTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,12 @@
 import {
     CreateTransactionController,
     GetTransactionsByUserIdController,
+    UpdateTransactionController,
 } from '../../controller';
 import {
     makeCreateTransactionController,
     makeGetTransactionsByUserIdController,
+    makeUpdateTransactionController,
 } from './transaction';
 
 describe('Transaction Controller Factories', () => {
@@ -16,6 +18,11 @@ describe('Transaction Controller Factories', () => {
     it('should return a valid GetTransactionsByUserIdController instance', () => {
         expect(makeGetTransactionsByUserIdController()).toBeInstanceOf(
             GetTransactionsByUserIdController,
+        );
+    });
+    it('should return a valid MakeUpdateTransactionController instance', () => {
+        expect(makeUpdateTransactionController()).toBeInstanceOf(
+            UpdateTransactionController,
         );
     });
 });
