2e94e0726a7a1d4e10114d4e5e6593a664013d30
Danilo Santos <dscofield@outlook.com.br>
2024-09-01 16:55:09 -0300

    test: ensure GET /api/users/:userId return 200 when user is found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -13,4 +13,18 @@ describe('Users Routes E2E Tests', () => {
 
         expect(response.statusCode).toBe(201);
     });
+
+    it('GET /api/users/:userId should return 200 when user is found', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        const response = await request(app).get(`/api/users/${createdUser.id}`);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(createdUser);
+    });
 });
