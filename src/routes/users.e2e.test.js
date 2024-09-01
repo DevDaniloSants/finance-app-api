a058b4d281b966e6bed89fcbbc322054576b74a9
Danilo Santos <dscofield@outlook.com.br>
2024-09-01 17:49:51 -0300

    test: ensure GET /api/users/:userId/balance return 200 and user balance

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker';
 
 import { app } from '../app.js';
 import { user } from '../tests/fixtures/user';
+import { TransactionType } from '@prisma/client';
 
 describe('Users Routes E2E Tests', () => {
     it('POST /users should return 201 when user is created', async () => {
@@ -70,5 +71,50 @@ describe('Users Routes E2E Tests', () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual(createdUser);
+    });
+
+    it('GET /api/users/:userId/balance return 200 and correct balance', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EARNING,
+            amount: 10000,
+        });
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.EXPENSE,
+            amount: 2000,
+        });
+
+        await request(app).post('/api/transactions').send({
+            user_id: createdUser.id,
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: TransactionType.INVESTMENT,
+            amount: 2000,
+        });
+
+        const response = await request(app).get(
+            `/api/users/${createdUser.id}/balance`,
+        );
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+            earnings: '10000',
+            expenses: '2000',
+            investiments: '2000',
+            balance: '6000',
+        });
     });
 });
