df68292576a1757e40ee190b03bc852f203bf707
Danilo Santos <dscofield@outlook.com.br>
2024-09-01 18:42:39 -0300

    feat: ensure UpdateUserRepository throws UserNotFoundError if user is not found

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,12 +1,23 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { prisma } from '../../../../prisma/prisma.js';
+import { UserNotFoundError } from '../../../errors/user.js';
 
 export class UpdateUserRepository {
     async execute(userId, updateUserParams) {
-        return await prisma.user.update({
-            where: {
-                id: userId,
-            },
-            data: updateUserParams,
-        });
+        try {
+            return await prisma.user.update({
+                where: {
+                    id: userId,
+                },
+                data: updateUserParams,
+            });
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    throw new UserNotFoundError(userId);
+                }
+            }
+            throw error;
+        }
     }
 }
