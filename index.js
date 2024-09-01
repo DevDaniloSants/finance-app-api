3775935282fda1f409067092e78c4778da153f0b
Danilo Santos <dscofield@outlook.com.br>
2024-09-01 15:48:53 -0300

    refactor: move transactions routes to separete file

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 
-import {
-    makeCreateTransactionController,
-    makeGetTransactionsByUserIdController,
-    makeUpdateTransactionController,
-    makeDeleteTransactionController,
-} from './src/factories/controllers/index.js';
-
-import { usersRouter } from './src/routes/users.js';
+import { usersRouter, transactionsRouter } from './src/routes/index.js';
 
 const app = express();
 
@@ -17,42 +10,6 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
-
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionsByUserController =
-        makeGetTransactionsByUserIdController();
-
-    const { statusCode, body } =
-        await getTransactionsByUserController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController();
-
-    const { statusCode, body } =
-        await createTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransactionController = makeUpdateTransactionController();
-
-    const { statusCode, body } =
-        await updateTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const deleteTransactionController = makeDeleteTransactionController();
-
-    const { statusCode, body } =
-        await deleteTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
+app.use('/api/transactions', transactionsRouter);
 
 app.listen(port, () => console.log(`listening to port ${port}`));
