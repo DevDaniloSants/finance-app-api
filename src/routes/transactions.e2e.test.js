55e4de5968637951bcc2f195897cb1ce38db33e7
Danilo Santos <dscofield@outlook.com.br>
2024-09-03 21:14:02 -0300

    test: ensure PATCH /api/transactions/:transactionId returns 200 when updating a transaction

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -54,4 +54,30 @@ describe('Transactions Routes  E2E Tests', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body[0].id).toBe(createdTransaction.id);
     });
+
+    it('PATCH /api/transactions/:transactionId should return 200 when updating a transaction successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({
+                ...transaction,
+                user_id: createdUser.id,
+                id: undefined,
+            });
+
+        const response = await request(app)
+            .patch(`/api/transactions/${createdTransaction.id}`)
+            .send({
+                amount: 100,
+            });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.amount).toBe('100');
+    });
 });
